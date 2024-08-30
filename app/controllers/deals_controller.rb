0b5866b99beb1d399d12fc94cb5c686e92e47c5f
Liam Simmons <liam.simmons@powerhrg.com>
2024-08-30 14:50:09 -0400

    create house form

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,6 +1,5 @@
 class DealsController < ApplicationController
   def index
-    byebug
     @deals = current_user.deals
   end
   def show
--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,6 +1,28 @@
 class HousesController < ApplicationController
   def index
+    @houses = House.all
+    @house = House.new
   end
+
   def show
+    @house = House.find(params[:id])
+  end
+
+  def new
+    @house = House.new
+  end
+
+  def create
+    @houses = House.all
+    @house = House.new(house_params)
+    if @house.save
+      redirect_to @house, notice: "House was successfully created."
+    else
+      render :index
+    end
+  end
+
+  def house_params
+    params.require(:house).permit(:address, :list_price, :annual_property_taxes, :year_built)
   end
 end
