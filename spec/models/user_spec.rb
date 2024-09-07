4468bed7d90fad7885a4c31823dad5d6f3c1fb6c
Liam Simmons <liam.simmons@powerhrg.com>
2024-09-07 10:54:23 -0400

    bootstrap

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
-# RSpec.describe User, type: :model do
-#   it { should have_many(:deals) }
-#   it { should validate_presence_of(:email) }
-#   it { should validate_uniqueness_of(:email) }
-#   it { should validate_presence_of(:encrypted_password) }
-# end
+RSpec.describe User, type: :model do
+  it { should have_many(:deals) }
+  it { should validate_presence_of(:email) }
+end
